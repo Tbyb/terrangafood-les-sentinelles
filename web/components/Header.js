755cb5bba9252ccef7f8d1c50b3a665863b71d0a
dialika740 <dialikandong740@gmail.com>
2026-04-21 13:53:57 +0000

    feat : ajout contributeur Dialika, ajouter le nom de l’équipe dans le composant

--- web/components/Header.js
+++ web/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="header">
       <div className="header-content">
         <Link href="/" className="header-logo">
-          🍛 Terranga<span>Food</span>
+          🍛 Terranga<span> Food - Les Sentilles</span>
         </Link>
         <nav className="header-nav">
           <Link href="/">Restaurants</Link>
