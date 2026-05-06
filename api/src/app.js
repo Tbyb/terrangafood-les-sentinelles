473fa6220c855ee236b0ce15bd7b6f9751c0c34d
dialika740 <dialikandong740@gmail.com>
2026-05-06 20:52:27 +0000

    fix: Ajout URL

--- api/src/app.js
+++ api/src/app.js
@@ -44,6 +44,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'https://terrangafood-les-sentinelles.vercel.app',
     process.env.FRONTEND_URL
   ].filter(Boolean),
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
